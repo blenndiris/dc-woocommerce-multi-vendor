1b2a1cce1bfa8507e00b4fcfc8bbf93afe9ee3e1
Jon Sakas <jon.sakas@gmail.com>
2020-05-05 16:33:49 -0600

    Add debug statement to vendor dashboard js

--- assets/frontend/js/wcmp_vendor_dashboard.js
+++ assets/frontend/js/wcmp_vendor_dashboard.js
@@ -1,5 +1,7 @@
 /*global wcmp_frontend_vdashboard_js_script_data */
 jQuery(document).ready(function($){
+	console.log('Leafz Vendor Dashboard');
+
 	$("body").on("click", ".wcmp_delate_announcements_dashboard", function(e) {
 			var post_id = $(this).attr("data-element");
 			var element_to_be_refrash = $(this).parent();	
